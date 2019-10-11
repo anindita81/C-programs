9a91ee4292eb4897401b444ab29fb64dc473cccc
anindita81 <32812562+anindita81@users.noreply.github.com>
2019-10-11 19:56:01 +0530

    Add files via upload

---
+++ Addition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf(Enter the number:);
+    scanf("%d",a);
+    printf(Enter the 2nd number:);
+    scanf("%d",b);
+    c=a+b;
+    printf(The sum is "%d",c);
+}
---
+++ Subtraction.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter both the numbers:")
+    scanf("%d %d",&a,&b);
+    c=a-b
+    printf("%d",c);
+}
